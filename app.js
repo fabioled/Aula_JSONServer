4f6a23ce02e281a2cf9daca3d5c7d7c7ebe50aad
Fábio Cordeiro <fabio@pucminas.br>
2024-05-22 14:54:02 -0300

    Adicionada a função de Editar

--- app.js
+++ app.js
@@ -25,7 +25,9 @@ document.getElementById('btnAddJogo').addEventListener('click', function () {
 function addJogoLista(jogo) {
     const listaJogos = document.getElementById('listaJogos');
     const li = document.createElement('li');
-    li.textContent = jogo.name;
+    const textSpan =  document.createElement('span');
+    textSpan.textContent = jogo.name;
+    li.appendChild(textSpan);
 
     //Criando botão de DELETAR
     const btnDelJogo = document.createElement('button');
@@ -33,6 +35,15 @@ function addJogoLista(jogo) {
     btnDelJogo.onclick = function() {
         deletarJogo(jogo.id, li);   
     }
+    li.appendChild(btnDelJogo);
+
+    //Criando botão de EDITAR
+    const btnEditJogo = document.createElement('button');
+    btnDelJogo.textContent = 'Editar';
+    btnDelJogo.onclick = function() {
+        editarJogo(jogo, textSpan);   
+    }
+    
     li.appendChild(btnDelJogo);
     listaJogos.appendChild(li);  
 }
@@ -47,6 +58,45 @@ function deletarJogo(jogoId, listaJogos) {
         }   
     });
 }
+
+//função editar jogo
+function editarJogo(jogo, textSpan){
+    const input = document.createElement('input');
+    input.type='text';
+    input.value = textSpan.textContent;
+    textSpan.parentNode.replaceChild(input, textSpan);
+
+    input.focus();
+
+    input.onblur = function() {
+        atualizarJogo(jogo.id, input.value, input, textSpan);
+    };
+
+    input.addEventListener('keypress', function(e) {
+        if (e.key === 'Enter') {
+            atualizarJogo(jogo.id, input.value, input, textSpan);
+        }
+    });
+}
+
+function atualizarJogo(jogoId, newValue, input, textSpan) {
+    if (newValue.trim() !== "") {
+        fetch(`http://localhost:3000/jogos/${jogoId}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ name: newValue })
+        }).then(response => response.json())
+          .then(updatedTask => {
+            textSpan.textContent = updatedTask.name;
+            input.parentNode.replaceChild(textSpan, input);
+        });
+    } else {
+        input.parentNode.replaceChild(textSpan, input);
+    }
+}
+
 
 //carregar os jogos do arquivo json existentes
 window.onload = function () {
